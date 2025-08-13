841998533e87985e9ac672555b43cccfc0a3d731
Pekrio <pekriopedro@gmail.com>
2025-08-13 01:36:47 -0300

    Cachorro que segue xD

--- main.py
+++ main.py
@@ -8,28 +8,36 @@ class Game:
         pygame.init()  # Inicializa os modulos do pygame
         self.width = 800
         self.height = 600
-        pygame.display.set_caption('Pedro, Morra!') # Nome da janela
+        pygame.display.set_caption("Pedro, Morra!")  # Nome da janela
         self.display_surface = pygame.display.set_mode(
             (self.width, self.height)
         )  # Cria a janela principal do jogo
         self.clock = pygame.Clock()
-        
+
         # Imagem do jogador (anti_bobby) redimensionada para 60x60
         self.jogador = pygame.image.load(join("anti_bobby.png"))
         self.jogador = pygame.transform.scale(self.jogador, (60, 60))
-        
+
+        # Imagem da bala
+        self.bala_imagem = pygame.image.load(join("bala.jpg"))
+        self.bala_imagem = pygame.transform.scale(self.bala_imagem, (30, 30))
+        self.bala_imagem_rect = self.bala_imagem.get_frect(
+            center=(self.width / 2, self.height / 2)
+        )
+        self.bala_ativa = 0
+
         # Imagem da seta redimensionada para 30x30
         self.seta_img = pygame.image.load(join("seta.png"))
         self.seta_img = pygame.transform.scale(self.seta_img, (30, 30))
-        
+
         # Direção do jogador e velocidade base
         self.jogador_direcao = pygame.Vector2()
         self.velocidade_jogador = 500
-        
+
         # Posiciona o jogador no centro da tela
         self.jogador_rect = self.jogador.get_frect(
-            center=(self.width / 2, self.height / 2)        
-            )  # Cria Rect da imagem no centro da tela        
+            center=(self.width / 2, self.height / 2)
+        )  # Cria Rect da imagem no centro da tela
         self.running = True  # Variavel de controle de fim de jogo/fechar a janela
         self.run()  # Inicializa a funçao run, para rodar o jogo
 
@@ -50,8 +58,7 @@ class Game:
             self.circle_anti_bobby()
             self.draw_arrow()  # Desenha a seta
             pygame.display.update()
-            print(self.mouse_pos)
-            
+
     def input(self, dt):  # Input WASD Básico
         keys = pygame.key.get_pressed()
         self.jogador_direcao[0] = (
@@ -75,7 +82,9 @@ class Game:
     def mouse(self):  # Rastreia o mouse
         self.mouse_pos = pygame.mouse.get_pos()  # Obtém a posição atual do mouse
         pygame.mouse.set_visible(True)  # Define o mouse como invisível
-        self.clicado = pygame.mouse.get_just_pressed()  # variavel de entrada e saida dos clicks do mouse
+        self.clicado = (
+            pygame.mouse.get_just_pressed()
+        )  # variavel de entrada e saida dos clicks do mouse
 
     def anti_bobby_collision(self):  # Define a colisão do antibobby
         if self.jogador_rect.bottom > self.height:
@@ -91,26 +100,62 @@ class Game:
             self.jogador_rect.left = 0
 
     def circle_anti_bobby(self):
-        pygame.draw.circle(self.display_surface, (255, 255, 255), self.jogador_rect.center, radius=38, width=1) # Circulo de colisão do anti_bobby
-        
+        pygame.draw.circle(
+            self.display_surface,
+            (255, 255, 255),
+            self.jogador_rect.center,
+            radius=38,
+            width=1,
+        )  # Circulo de colisão do anti_bobby
+
+    def cachorro_segue_mouse(self):
+        if pygame.mouse.get_pressed()[0]:
+            self.bala_ativa = 1
+        if self.bala_ativa == 1:
+            self.display_surface.blit(self.bala_imagem, self.bala_imagem_rect)
+            if abs(self.bala_imagem_rect.x - self.mouse_pos[0]) > 1:
+                if self.bala_imagem_rect.x < self.mouse_pos[0]:
+                    self.bala_imagem_rect.centerx += 2
+                elif self.bala_imagem_rect.x > self.mouse_pos[0]:
+                    self.bala_imagem_rect.centerx -= 2
+
+            if abs(self.bala_imagem_rect.y - self.mouse_pos[1]) > 1:
+                if self.bala_imagem_rect.y < self.mouse_pos[1]:
+                    self.bala_imagem_rect.centery += 2
+                elif self.bala_imagem_rect.y > self.mouse_pos[1]:
+                    self.bala_imagem_rect.centery -= 2
+
+            dx = self.mouse_pos[0] - self.bala_imagem_rect.x
+            dy = self.mouse_pos[1] - self.bala_imagem_rect.y
+            distance = math.sqrt(dx * dx + dy * dy)
+            if distance < 1.5:
+                self.running = False
+            print(distance)
+
     def draw_arrow(self):
         # Calcula a distância entre o personagem e o mouse
         dx = self.mouse_pos[0] - self.jogador_rect.centerx
         dy = self.mouse_pos[1] - self.jogador_rect.centery
-        distance = math.sqrt(dx*dx + dy*dy)
-        
-        angulo_radiano= math.atan2(dy,dx) #retorna o angulo em radianos entre o jogador e o mouse
-        angulo_graus= math.degrees(angulo_radiano) #transforma para graus
-        seta_rotacionada=pygame.transform.rotate(self.seta_img, -angulo_graus -90) #posição da seta para vetical
-        
-        valor_cosseno=math.cos(angulo_radiano)
-        valor_seno=math.sin(angulo_radiano) #calcula o seno e o cosseno 
-        
+        distance = math.sqrt(dx * dx + dy * dy)
+
+        angulo_radiano = math.atan2(
+            dy, dx
+        )  # retorna o angulo em radianos entre o jogador e o mouse
+        angulo_graus = math.degrees(angulo_radiano)  # transforma para graus
+        seta_rotacionada = pygame.transform.rotate(
+            self.seta_img, -angulo_graus - 90
+        )  # posição da seta para vetical
+
+        valor_cosseno = math.cos(angulo_radiano)
+        valor_seno = math.sin(angulo_radiano)  # calcula o seno e o cosseno
+
         pos_x = self.jogador_rect.centerx + 38 * valor_cosseno
-        pos_y = self.jogador_rect.centery + 38 * valor_seno #define a posição da seta 
-        
-        self.display_surface.blit(seta_rotacionada, seta_rotacionada.get_rect (center=(pos_x, pos_y))) #coloca a seta na tela com as informações desenvolvidas
-       
+        pos_y = self.jogador_rect.centery + 38 * valor_seno  # define a posição da seta
+
+        self.display_surface.blit(
+            seta_rotacionada, seta_rotacionada.get_rect(center=(pos_x, pos_y))
+        )  # coloca a seta na tela com as informações desenvolvidas
+        self.cachorro_segue_mouse()
+
+
 Game()
-
-
